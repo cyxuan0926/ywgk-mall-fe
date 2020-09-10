c5971f8b557a6e038a6eb81889bc3e5a12014d0d
cy0926 <840473997@qq.com>
2020-09-10 16:03:40 +0800

    refactor: 最新代码

--- static/config/conf.js
+++ static/config/conf.js
@@ -2,5 +2,5 @@ window.PLATFORM = 'CUSTOMER';
 window.VERSION = '0.1.0-dev';
 // window.AUTH_BASE_URL = 'http://10.10.10.17:8081/';
 // window.TRADE_BASE_URL = 'http://10.10.10.17:8084/';
-window.AUTH_BASE_URL = 'http://api.auth.prisonpublic.com/';
+window.AUTH_BASE_URL = 'http://qa-auth-api.yuwugongkai.com/';
 window.TRADE_BASE_URL = 'http://api.trade.prisonpublic.com/';
